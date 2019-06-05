47b5cac4ed492cc16a2d27e4cfaef731b036277e
david.wang <david.wang@autoflight.com>
2019-06-05 17:35:00 +0800

    tokio init

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
+#![deny(deprecated)]
+extern crate tokio;
 
-struct Cli {
-    s:String,
-    num:u32,
-}
-
-fn test() {
-    println!("another print");
-}
+use tokio::net::TcpStream;
 
 fn main() {
-    println!("Hello, world!");
-    let mut x = 5;
-    println!("number of x: {}", x);
-    x = 6;
-    println!("number of x: {}", x);
-    let mut _x = Cli {s: "str".to_string(), num: 50};
-    println!("number of _x: {} and {}", _x.s, _x.num);
-    
-    test();
+    // Parse the address of whatever server we're talking to
+    let addr = "127.0.0.1:6142".parse().unwrap();
+    let client = TcpStream::connect(&addr);
+
+    // Following snippets come here...
 }
